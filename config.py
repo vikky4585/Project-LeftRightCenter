17a06c84d1dc551e673d5dd8134920151b85bc51
Vikas <vikassingheng@gmail.com>
2018-12-23 18:23:46 -0600

    Draft version of twitter data

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ consumer_key = "N7O7uH6Khdhfmx7qWi5Vr8bnN"
 consumer_secret = "JcNhgLsZLkQJh2i50DLdudU2vPmlfNfKsDG7WwfNXp8Ujk2DDk"
 access_token = "1038597394911440899-tkKMwCjeiyXfMuCf1j19Gc62YzF9ho"
 access_token_secret = "p10MTak63JVMEqYcJbnReaA2SUjieV5u5edmj698JIQdD"
+
+x_api_key = "5c1d0439e3de7d040e0ec5f5"
+x_api_secret_key="mboyppmWy2ZG9kfQ4Cyn22ur83enOFurQgjDxPhVa4M"
