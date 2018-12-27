fd51abced4c9b3e637e2c9e0cda9a05b5c6f61c8
Vikas <vikassingheng@gmail.com>
2018-12-27 12:27:36 -0600

    code refactoring

---
+++ PredictorNeuralNetworkFunction.py
@@ -0,0 +1,58 @@
+
+#%%
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+#%%
+
+
+
+#%%
+
+def runPredictor(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=1, stratify=y)
+
+    label_encoder = LabelEncoder()
+    label_encoder.fit(y_train)
+
+    y_train_encoded = label_encoder.transform(y_train)
+    y_test_encoded = label_encoder.transform(y_test)
+
+    y_train_category = to_categorical(y_train_encoded)
+    y_test_category = to_categorical(y_test_encoded)
+
+
+#%%
+
+
+    model = Sequential()
+    model.add(Dense(units=100,activation='relu', input_dim=X.shape[1]))
+    model.add(Dense(units=100,activation='relu'))
+    model.add(Dense(units=2, activation='softmax'))
+            
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(X_train, y_train_category,epochs=100, shuffle=True,verbose=2)
+
+
+    #%%
+    model_loss, model_accuracy = model.evaluate(
+        X_test, y_test_category, verbose=2)
+    print(
+        f"Normal Neural Network - Loss: {model_loss}, Accuracy: {model_accuracy}")
+
+
+    #%%
+    predictions = model.predict_classes(X_test[:10])
+    prediction_labels = label_encoder.inverse_transform(predictions)
+    prediction_labels
+
+    print(f"Predicted classes: {prediction_labels}")
+    print(f"Actual Labels: {list(y_test[:10])}")
+
+
