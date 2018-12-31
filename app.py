e45832b08167bc714af5f5b27f29439955a7e75a
Vikas <vikassingheng@gmail.com>
2018-12-31 13:29:02 -0600

    template website

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, render_template
+import pymongo
+import re
+
+
+
+app = Flask(__name__)
+
+#conn = 'mongodb://localhost:27017'
+conn = 'mongodb://edwardwisejr:Flender77!@ds255403.mlab.com:55403/manufacture_consent'
+client = pymongo.MongoClient(conn)
+
+#db = client['buzzword']
+db = client.get_database()
+#db_details = db['twitter']
+db_details = db['mc_tweets_to_plot']
+
+
+@app.route("/")
+def home():
+    return render_template("landingpage.html")
+
+@app.route("/dnn")
+def mapwi():
+    return render_template("dnn.html")
+
+@app.route("/buzzwordmap/<buzzword>")
+def buzzwordmap(buzzword):
+    print(f'map buzz word {buzzword}')
+    return render_template("map_buzzwordmap.html", buzzword=buzzword)
+
+
+@app.route("/getdata/<buzzword>")
+def getdata(buzzword):
+    print(f'buzz word getdata {buzzword}')
+    rgx = re.compile(f'.*{buzzword}.*', re.IGNORECASE)
+
+    fetchData = db_details.find({'buzz_word': rgx})
+    #fetchData = db_details.find()
+
+    dataList = []
+    for x in fetchData:
+        obj = {}
+        obj['buzzword'] = x['buzz_word']
+        obj['date'] = x['date']
+        obj['lat'] = x['lat']
+        obj['lon'] = x['long']
+        obj['source'] = x['source']
+        dataList.append(obj)
+    print(f'Rendering  data {dataList}')
+    return jsonify(dataList)
+
+
+@app.route("/trends/<buzzword>")
+def trends(buzzword):
+    print(f'trends buzz word {buzzword}')
+    return render_template("plot.html", buzzword=buzzword)
+
+@app.route("/getplotdata/<buzzword>")
+def plotData(buzzword):
+
+    return getdata(buzzword)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/map_heat.js
@@ -0,0 +1,125 @@
+
+// Creating map object
+var heatMap = L.map("heatmap", {
+  center: [44.7844, -88.7879],
+  zoom: 7,
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(heatMap);
+
+function createRandom(value, count) {
+
+  let randomList = [];
+  for (let i = 0; i < count; i++) {
+
+    randomList.push([+value[0] + Math.random() / 1000, +value[1] + Math.random() / 1000])
+  }
+
+  return randomList;
+}
+var buzzword = d3.select("p").text()
+console.log("fetched buzzword from html " + buzzword);
+//TODO temp hack
+// if (buzzword === 'clinton') {
+//   buzzword = "hillary's email";
+// }
+let url = `/getdata/${buzzword}`
+console.log("url " + url)
+// Grabbing our GeoJSON data..
+d3.json(url, function (data) {
+
+  let heatArray = [];
+
+  for (let i = 0; i < data.length; i++) {
+    //heatArray.push([data[i].lat, data[i].lon])
+    let cnt = 1000;
+    if (buzzword === "wall") {
+      cnt = 100;
+    }
+    let lst = [];
+    if ((data[i].lat > 43 && data[i].lat < 45)) {
+      lst = createRandom([data[i].lat, data[i].lon], cnt);
+
+    }
+
+
+    for (let j = 0; j < lst.length; j++) {
+
+      if (lst[i] != null) {
+        heatArray.push([lst[i][0], lst[i][1]])
+      }
+
+
+    }
+
+  }
+
+  L.heatLayer(heatArray, {
+    radius: 40,
+    blur: 55,
+    gradient: { 0.4: 'blue', 0.65: 'lime', 1: 'red' }
+  }).addTo(heatMap);
+
+
+
+  d3.json("../static/data/WI.geojson", function (data) {
+    L.geoJson(data, {
+      style: function (feature) {
+
+        return {
+          color: "black",
+          weight: 0.5
+        }
+      },
+      onEachFeature: function (feature, layer) {
+
+        layer.on({
+          mouseover: function (event) {
+            layer = event.target;
+          }
+        })
+        layer.bindPopup("<h5>" + feature.properties.COUNTY_NAME + "</h5>"
+        );
+
+      }
+    }
+
+    ).addTo(heatMap);
+  });
+
+
+
+});
+
+var legend = L.control({ position: 'topright' });
+
+legend.onAdd = function (map) {
+
+  var div = L.DomUtil.create('div', 'info legend');
+  div.innerHTML += '<h6><i>US Presidential Election 2016 - Wisconsin</i></h6> '
+  div.innerHTML += '<br><i>Twitter Heatmap for buzzword :  '
+
+  if ("clinton" === buzzword) {
+    div.innerHTML += 'Hillary Clinton Emails</i> '
+
+  } else {
+    div.innerHTML += buzzword + '</i> '
+
+  }
+  return div;
+};
+
+legend.addTo(heatMap);
+
+
+
+
+
+
+
