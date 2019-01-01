f8a1f968018932c4f19328689d79b07547eb03c3
Vikas <vikassingheng@gmail.com>
2019-01-01 14:45:04 -0600

    app.py prediction functionality

--- app.py
+++ app.py
@@ -1,12 +1,86 @@
-from flask import Flask, jsonify, render_template, redirect, request
-
+import json
 import re
+from datetime import datetime
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import tweepy
+from flask import Flask, jsonify, redirect, render_template, request
 
+from config import (access_token, access_token_secret, consumer_key,
+                    consumer_secret, x_api_key, x_api_secret_key)
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+url='https://api-v3.receptiviti.com/v3/api/content'
+headers={'X-API-KEY': x_api_key, 'X-API-SECRET-KEY':x_api_secret_key}
 
 app = Flask(__name__)
 
+def payload(text):
+    payload = {}
+    tags = []
+    tags.append('string')
+    payload['content_tags'] = tags
+    payload['content_handle'] = 'string'
+    payload['language'] = 'english'
+    payload['content_source'] = 4
+    payload['content_date'] = '2018-12-25T00:46:05.119779'
+    payload['recipient_id'] = 'string'
+    payload['language_content'] = text
+    return payload
 
+def gettweets(target_user):
+    tweets = []
+
+    for x in range(1, 26):
+        
+        public_tweets = api.user_timeline(target_user, page=x, tweet_mode='extended')
+
+        for twt in public_tweets:
+            try:
+                details = {}
+            
+                details['created_at'] = twt['created_at']
+                details['screen_name'] = twt['user']['screen_name']
+                #details['text'] = twt['text']
+                #details['full_text'] = twt['extended_tweet']['full_text']
+                details['full_text'] = twt['full_text']
+                
+                tweets.append(details)
+            except tweepy.TweepError:
+                print(f'skipping for {target_user}')
+            
+    return tweets
+
+def cleanText(x):
+    x = x.replace('RT','')
+    x = x.replace(' .','.')
+    x = x.replace('. ','.')
+    x = re.sub('[^a-zA-Z0-9 \n\.]', '', x)
+    x = re.sub(r'http\S+', '', x)
+    x = re.sub(r'\w+:\s?', '', x)
+    return x
+
+def sendLIWC(text, party, userid):
+    
+    print(f'sending request for userid {userid}' )
+
+    response = requests.post(url, headers=headers,json=payload(text))
+    liwc = {}
+    liwc['userid'] = userid
+    liwc['party'] = party
+    raw_score = dict(liwc)
+    raw_score.update(response.json()['receptiviti_scores']['raw_scores'])
+    percentile_score = dict(liwc)
+    percentile_score.update(response.json()['receptiviti_scores']['percentiles'])
+    category_score = dict(liwc)
+    category_score.update(response.json()['liwc_scores']['categories'])
+    
 
 
 @app.route("/")
@@ -22,6 +96,16 @@ def predict():
     handle = request.form['handle']
     algoname = request.form['algoname']
     print(f'Predicting for handle: {handle} via algorithm: {algoname}')
+    tweetlist = gettweets(handle)
+    df = pd.DataFrame(tweetlist)
+    df['full_text_formatted'] = df['full_text'].apply(cleanText)
+    text = ''
+    for t in df['full_text_formatted']:
+        text = text + t + '.'
+    print('sending text to liwc api')
+
+
+    #return a jsonify version of preidctons and other data
     return render_template("dnn.html")
 
 @app.route("/buzzwordmap/<buzzword>")
